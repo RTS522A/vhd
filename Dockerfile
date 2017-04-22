1c7992f2bbfc050c2f7ef0043eef5d3e5943bfcf
Jimmy Xu <xjimmyshcn@gmail.com>
2017-04-22 22:33:39 +0800

    [master] add virtio-win driver

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,19 @@ RUN powershell -Command \
   $ErrorActionPreference = 'Stop'; \
 	c:\hyper\hyperstart.exe -install;
 
+# add virtio driver
+ADD virtio-win /virtio-win
+
+# install virtio driver
+RUN powershell -Command \
+  $ErrorActionPreference = 'Stop'; \
+	pnputil /add-driver 'c:\virtio-win\Balloon\2k16\amd64\balloon.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\NetKVM\2k16\amd64\netkvm.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\pvpanic\2k16\amd64\pvpanic.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\vioinput\2k16\amd64\vioinput.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\viorng\2k16\amd64\viorng.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\vioscsi\2k16\amd64\vioscsi.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\vioserial\2k16\amd64\vioser.inf' /install; \
+	pnputil /add-driver 'c:\virtio-win\viostor\2k16\amd64\viostor.inf' /install
+
 CMD [ "ping localhost -t" ]
--- build.sh
+++ build.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
+###################################################################
+echo "----- Download virtio-win -----"
+curl -C - -O -s https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+
+echo "----- mount virtio-win.iso -----"
+PowerShell  -Command  Mount-DiskImage  -ImagePath  $PWD/virtio-win.iso
+
+echo "----- Get Virtual CDROM driver letter -----"
+ISODRV=$(powershell -Command Get-Volume | grep virtio-win | awk '{print $1}' | head -n1)
+if [ ${ISODRV} == "" ];then
+  echo "Can not mount virtio-win.iso, quit"
+  exit 1
+fi
+
+echo "----- Find Driver for win 2k16 -----"
+find "/${ISODRV}/" | grep 2k16$
+
+echo "----- Cpoy Driver for win 2k16 -----"
+mkdir -p virtio-win
+for i in $(find "/${ISODRV}/" | grep 2k16$)
+do
+  DIRDRV=$(echo $i | cut -d'/' -f3)
+  mkdir -p virtio-win/${DIRDRV}/2k16
+  cp $i/* virtio-win/${DIRDRV}/2k16/ -rf
+done
+ls virtio-win
+
+echo "----- dismount virtio-win.iso -----"
+PowerShell  -Command  Dismount-DiskImage  -ImagePath  $PWD/virtio-win.iso
+
+###################################################################
 echo "----- Build -----"
 docker build -t xjimmyshcn/nanoserver-hyper .
